a39c3bebef9afa05c228ff7d8412127e25b60b89
NavyaLikhita <54634629+NavyaLikhita@users.noreply.github.com>
2019-09-24 18:24:06 +0530

    Add files via upload

---
+++ SpringCore/src/main/java/com/cg/springcore/service/Department.java
@@ -0,0 +1,18 @@
+package com.cg.springcore.service;
+
+public class Department {
+
+	private int depId;
+
+	public int getDepId() {
+		return depId;
+	}
+
+	public void setDepId(int depId) {
+		this.depId = depId;
+	}
+
+	
+
+	
+}
---
+++ SpringCore/src/main/java/com/cg/springcore/service/Employee.java
@@ -0,0 +1,48 @@
+package com.cg.springcore.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+@Component("emp")
+public class Employee {
+	
+	@Value("ABC")
+	private String empName;
+	@Autowired
+	private Project myPro;
+	
+	
+	public Project getMyPro() {
+		return myPro;
+	}
+
+
+	public void setMyPro(Project myPro) {
+		this.myPro = myPro;
+	}
+
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+
+	public void getData() {
+		
+		System.out.println("Hii.....!!!!!"+this.empName+" "+myPro.getProjId());
+		
+	}
+
+	
+	
+	
+	
+	
+
+}
